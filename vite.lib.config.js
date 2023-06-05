c54d84b63ce75d1ce3e00d125d086ebb9cf120e4
Erik Pukinskis <erik@snowedin.net>
2023-06-05 17:50:11 -0600

    Fix externals to include all of firebase/*

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -27,12 +27,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react", "firebase"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: { "react": "react", "firebase": "firebase" },
-      },
+      external: ["react", "firebase", /firebase\/.+/],
     },
   },
 })
