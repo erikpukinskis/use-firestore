e08d84c749b19a3b43fd574b977945ab6590cb49
Erik Pukinskis <erik@snowedin.net>
2023-06-05 21:24:28 -0600

    Add yarn zero install

--- docs/index.tsx
+++ docs/index.tsx
@@ -1,7 +1,7 @@
+import * as HomePage from "./HomePage.docs"
 import { DocsApp } from "codedocs"
 import React from "react"
 import { render } from "react-dom"
-import * as HomePage from "./HomePage.docs"
 
 render(
   <DocsApp icon="fire" logo="Firestore Hooks" docs={[HomePage]} />,
